a71c6c2a0d70ca31809c2de231fdcbc99d074f37
Hojjat Seyed <hojjatseyed@gmail.com>
2019-12-19 14:59:29 +0100

    Added base for System

---
+++ Prog3Assignment/System.cpp
@@ -0,0 +1,15 @@
+#include "System.hpp"
+System::System() {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    win = SDL_CreateWindow("Game", 10, 10, 700, 500, 0);
+    ren = SDL_CreateRenderer(win, -1, 0);
+}
+
+/*System::~System() {
+    SDL_DestroyWindow(win);
+    SDL_DestroyRenderer(ren);
+    SDL_Quit();
+}*/
+
+System sys;
+
---
+++ Prog3Assignment/System.hpp
@@ -0,0 +1,18 @@
+#ifndef System_hpp
+#define System_hpp
+
+#include <SDL2/SDL.h>
+struct System
+{
+    System();
+    //~System();
+    
+    SDL_Window* win;
+    SDL_Renderer* ren;
+    
+    
+};
+extern System sys;
+
+
+#endif /* System_hpp */
