45c2484afb7e50d6907270547ac4be67a25f03af
Hojjat Seyed <hojjatseyed@gmail.com>
2019-12-19 13:48:08 +0100

    Initial Commit

---
+++ Prog3Assignment/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Prog3Assignment
+//
+//  Created by Hojjat Seyed on 2019-12-19.
+//  Copyright © 2019 Hojjat Seyed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
