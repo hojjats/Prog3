1aa34307a06630b4d6b69011af7789210e2b61b9
Stuix <45558094+Stuix@users.noreply.github.com>
2019-12-20 14:28:26 +0100

    Added sprite

---
+++ Prog3Assignment/Sprite.cpp
@@ -0,0 +1,15 @@
+#include "Sprite.hpp"
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+
+class Sprite{
+public:
+    virtual void draw() const = 0;
+    SDL_Rect getRect() const { return rect; }
+    virtual void tick() = 0;
+
+protected:
+    Sprite(int x, int y, int w, int h) : rect{ x,y,w,h } {}
+    SDL_Rect rect;
+};
---
+++ Prog3Assignment/Sprite.hpp
@@ -0,0 +1,7 @@
+#ifndef SPRITE_HPP
+#define SPRITE_HPP
+
+
+
+
+#endif
