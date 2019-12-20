259b2dd131ac5ef97038c8b888fd8828fa642dfc
Hojjat Seyed <hojjatseyed@gmail.com>
2019-12-20 14:43:21 +0100

    Added handleEvent in sprite header and deleted old code in sprite.cpp

--- Prog3Assignment/Sprite.hpp
+++ Prog3Assignment/Sprite.hpp
@@ -1,7 +1,6 @@
 #ifndef SPRITE_HPP
 #define SPRITE_HPP
 
-#include <stdio.h>
 #include <SDL2/SDL.h>
 
 class Sprite{
@@ -9,6 +8,7 @@ public:
     virtual void draw() const = 0;
     SDL_Rect getRect() const { return rect; }
     virtual void tick() = 0;
+    virtual void handleEvent(SDL_Event);
 
 protected:
     Sprite(int x, int y, int w, int h) : rect{ x,y,w,h } {}
