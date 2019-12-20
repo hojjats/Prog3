6958148ea48fc9ff3d5015dccf57532ceb152b80
Stuix <45558094+Stuix@users.noreply.github.com>
2019-12-20 15:14:06 +0100

    destructor

--- Prog3Assignment/Sprite.hpp
+++ Prog3Assignment/Sprite.hpp
@@ -8,7 +8,7 @@ public:
     virtual void draw() const = 0;
     SDL_Rect getRect() const { return rect; }
     virtual void tick() = 0;
-    
+    virtual ~Sprite();
 protected:
     Sprite(int x, int y, int w, int h) : rect{ x,y,w,h } {}
     SDL_Rect rect;
