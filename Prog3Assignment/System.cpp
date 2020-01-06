3ce1575936d2adb379a033cb4001977a109b906d
Hojjat Seyed <hojjatseyed@gmail.com>
2020-01-06 12:20:53 +0100

    SDL Mixer added

--- Prog3Assignment/System.cpp
+++ Prog3Assignment/System.cpp
@@ -3,9 +3,14 @@ System::System() {
     SDL_Init(SDL_INIT_EVERYTHING);
     win = SDL_CreateWindow("Game", 10, 10, 700, 500, 0);
     ren = SDL_CreateRenderer(win, -1, 0);
+    Mix_OpenAudio(20050, AUDIO_S16SYS, 2, 4096);
+    music = Mix_LoadWAV(__FILE_NAME__);
+    Mix_PlayChannel(-1, music, -1);
 }
 
 System::~System() {
+    Mix_FreeChunk(music);
+    Mix_CloseAudio();
     SDL_DestroyWindow(win);
     SDL_DestroyRenderer(ren);
     SDL_Quit();
