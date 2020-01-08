989eb40beab40394de42ef1d2b5365ab49341ec1
gustavjunedahl <gustav.junedahl@gmail.com>
2020-01-08 13:36:38 +0100

    Added BirdGame

---
+++ Prog3Assignment/BirdGame.cpp
@@ -0,0 +1,13 @@
+//
+//  BirdGame.cpp
+//  Prog3Assignment
+//
+//  Created by Gustav Junedahl on 2020-01-08.
+//  Copyright © 2020 Hojjat Seyed. All rights reserved.
+//
+
+#include "BirdGame.hpp"
+
+void BirdGame::start() {
+    
+}
---
+++ Prog3Assignment/BirdGame.hpp
@@ -0,0 +1,27 @@
+//
+//  BirdGame.hpp
+//  Prog3Assignment
+//
+//  Created by Gustav Junedahl on 2020-01-08.
+//  Copyright © 2020 Hojjat Seyed. All rights reserved.
+//
+
+#ifndef BirdGame_hpp
+#define BirdGame_hpp
+
+#include "GameEngine.hpp"
+#include "Bird.hpp"
+#include "Pipe.hpp"
+#include "Ground.hpp"
+#include "Background.hpp"
+#include "Text.hpp"
+
+#include <stdio.h>
+
+class BirdGame {
+public:
+    void start();
+private:
+};
+
+#endif /* BirdGame_hpp */
